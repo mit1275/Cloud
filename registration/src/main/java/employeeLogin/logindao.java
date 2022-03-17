3eb8bae85f604b21c43cc07a1c7c30da7599d83a
Amit Kumar Upadhyay <66126457+mit1275@users.noreply.github.com>
2022-03-17 19:46:08 +0530

    Update logindao.java

--- registration/src/main/java/employeeLogin/logindao.java
+++ registration/src/main/java/employeeLogin/logindao.java
@@ -7,11 +7,14 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.servlet.http.HttpSession;
+
 public class logindao {
 	String url="jdbc:mysql://localhost:3306/employees";
 	String sqlusername="root";
 	String sqlpass="rootpassword1";
 	String query="select * from amit_111915014_detail where username=? and password=?";
+	
 	public boolean check(String uname,String pass) throws ClassNotFoundException, SQLException {
 	
 		Class.forName("com.mysql.jdbc.Driver");
@@ -24,6 +27,27 @@ public class logindao {
 
 		ResultSet rs=st.executeQuery();
 		if(rs.next()) {
+
+			String q2="select * from amit_111915014_salary where id=`${query.id}`";
+			
+			PreparedStatement st1=con.prepareStatement(q2);
+			
+			 ResultSet rs1=st1.executeQuery(q2);
+		        if(rs.next())
+		        {
+
+					HttpSession session = request.getSession();
+					session.setAttribute("username", uname);
+					response.sendRedirect("welcome.jsp");
+		          }
+		       else
+		       {
+		    	   response.sendRedirect("login.jsp");
+		       }
+		     con.close();
+		     }
+		    
+			
 			return true;
 		}
 		return false;
